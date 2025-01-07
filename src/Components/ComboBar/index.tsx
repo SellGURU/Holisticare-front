eb3b06696aa12b59bb1fb9fd98138f373abb9b82
SmurfsNIma <sadeghinima090@gmail.com>
2025-01-07 14:35:51 +0330

    add expert note

--- src/Components/ComboBar/index.tsx
+++ src/Components/ComboBar/index.tsx
@@ -10,6 +10,7 @@ import { ButtonPrimary } from "../Button/ButtonPrimary.tsx";
 import { useConstructor } from "../../help.ts";
 import { useFormik } from "formik";
 import Data from "./data.json";
+import Accordion from "../Accordion/index.tsx";
 export const ComboBar = () => {
   const { id } = useParams<{ id: string }>();
   const itemList = [
@@ -20,7 +21,7 @@ export const ComboBar = () => {
       name: "Questionary Tracking",
       url: "/icons/sidbar-menu/clipboard-text.svg",
     },
-    { name: "Notes", url: "/icons/sidbar-menu/note-2.svg" },
+    { name: "Expert’s Note", url: "/icons/sidbar-menu/note-2.svg" },
     { name: "Repeat", url: "/icons/sidbar-menu/repeat.svg" },
     { name: "Tasks", url: "/icons/sidbar-menu/task-square.svg" },
     { name: "Timeline", url: "/icons/sidbar-menu/timeline.svg" },
@@ -30,7 +31,7 @@ export const ComboBar = () => {
     email: "",
     picture: "",
   });
-  const [data] = useState<any>(Data);
+  const [data, setData] = useState<any>(Data);
 
   useEffect(() => {
     Application.getPatientsInfo({
@@ -111,6 +112,8 @@ export const ComboBar = () => {
     setIsSlideOutPanel(true);
   };
   const [activeItem, setActiveItem] = useState<string | null>(null);
+  const [showAddNote, setShowAddNote] = useState(false);
+  const [commentText, setCommentText] = useState("");
   const renderModalContent = () => {
     switch (activeItem) {
       case "Client Info":
@@ -244,54 +247,63 @@ export const ComboBar = () => {
           </div>
         );
       case "File History":
-        return <div className=" w-full">
-        <div className="px-2">
-          <div className="w-full text-[12px] px-5 py-3 h-[48px] border border-Gray-50 bg-backgroundColor-Main text-Primary-DeepTeal font-medium  flex justify-between items-center rounded-[12px]">
-            <div>File Name</div>
-            <div>Upload Date</div>
-            <div>Action</div>
-          </div>
-
-          <>
-            {data["File History"]?.length > 0 ? (
-              <>
-                <div className="flex justify-center w-full items-start overflow-auto h-[240px]">
-                  <div className="w-full mt-2">
-                    {data["File History"]?.map((el: any) => {
-                      return (
-                        <div className=" bg-white border border-Gray-50 mb-1 p-3 h-[48px] w-full rounded-[12px] flex justify-between items-center text-Text-Primary text-[10px]">
-                          <div className="text-[10px] w-[50px]  text-Text-Primary">
-                            {el.Data}
-                          </div>
-                          <div className="w-[70px] text-center">
-                            {el["Upload Date"]} 
-                          </div>
-                          <div className="flex items-center justify-end gap-1">
-                            <img  className="cursor-pointer" src="/icons/eye-green.svg" alt="" />
-                            <img className="cursor-pointer"  src="/icons/import.svg" alt="" />
-                          </div>
-                        
-                        </div>
-                      );
-                    })}
-                  </div>
-                </div>
-              </>
-            ) : (
-              <div className="flex flex-col items-center justify-center h-[200px]">
-                <img
-                  className=" object-contain"
-                  src="/icons/document-text.svg"
-                  alt=""
-                />
-                <div className="text-[12px] text-[#383838]">
-                  No Data Found
-                </div>
+        return (
+          <div className=" w-full">
+            <div className="px-2">
+              <div className="w-full text-[12px] px-5 py-3 h-[48px] border border-Gray-50 bg-backgroundColor-Main text-Primary-DeepTeal font-medium  flex justify-between items-center rounded-[12px]">
+                <div>File Name</div>
+                <div>Upload Date</div>
+                <div>Action</div>
               </div>
-            )}
-          </>
-        </div>
-      </div>
+
+              <>
+                {data["File History"]?.length > 0 ? (
+                  <>
+                    <div className="flex justify-center w-full items-start overflow-auto h-[240px]">
+                      <div className="w-full mt-2">
+                        {data["File History"]?.map((el: any) => {
+                          return (
+                            <div className=" bg-white border border-Gray-50 mb-1 p-3 h-[48px] w-full rounded-[12px] flex justify-between items-center text-Text-Primary text-[10px]">
+                              <div className="text-[10px] w-[50px]  text-Text-Primary">
+                                {el.Data}
+                              </div>
+                              <div className="w-[70px] text-center">
+                                {el["Upload Date"]}
+                              </div>
+                              <div className="flex items-center justify-end gap-1">
+                                <img
+                                  className="cursor-pointer"
+                                  src="/icons/eye-green.svg"
+                                  alt=""
+                                />
+                                <img
+                                  className="cursor-pointer"
+                                  src="/icons/import.svg"
+                                  alt=""
+                                />
+                              </div>
+                            </div>
+                          );
+                        })}
+                      </div>
+                    </div>
+                  </>
+                ) : (
+                  <div className="flex flex-col items-center justify-center h-[200px]">
+                    <img
+                      className=" object-contain"
+                      src="/icons/document-text.svg"
+                      alt=""
+                    />
+                    <div className="text-[12px] text-[#383838]">
+                      No Data Found
+                    </div>
+                  </div>
+                )}
+              </>
+            </div>
+          </div>
+        );
       case "Questionary Tracking":
         return (
           <div className=" w-full">
@@ -371,6 +383,134 @@ export const ComboBar = () => {
                 )}
               </>
             </div>
+          </div>
+        );
+      case "Expert’s Note":
+        return (
+          <div className=" w-full ">
+            <div className="w-full flex justify-between px-3.5 py-3">
+              {/* <div className="text-[14px] text-light-secandary-text dark:text-[#FFFFFFDE]">
+                Trainer's Notes (
+                {data?.notes ? data.notes.length : "0"})
+              </div> */}
+              {!showAddNote && (
+                <div
+                  onClick={() => {
+                    setShowAddNote(true);
+                  }}
+                  className="text-[14px] flex cursor-pointer justify-center items-center gap-1 bg-white border-Primary-DeepTeal border rounded-xl border-dashed px-5 py-2 w-full text-Primary-DeepTeal "
+                >
+                  <img className="w-6 h-6" src="/icons/add-blue.svg" alt="" />
+                  Add Note
+                </div>
+              )}
+            </div>
+            {showAddNote ? (
+              <div className="flex justify-center items-center">
+                <div className="w-full ">
+                  <div className="text-[12px] font-medium text-Text-Primary ">
+                    Note
+                  </div>
+                  <textarea
+                    value={commentText}
+                    onChange={(e) => {
+                      setCommentText(e.target.value);
+                    }}
+                    placeholder="Add your note for this client here..."
+                    className="h-[215px] font-light text-[12px] p-2 border border-Gray-50  mt-1 rounded-[16px] bg-backgroundColor-Card w-full resize-none outline-none"
+                  />
+                  <div className="flex justify-between items-center mt-2">
+                    <ButtonPrimary
+                      ClassName="bg-backgroundColor-Main"
+                      onClick={() => {
+                        setShowAddNote(false);
+                        setCommentText("");
+                      }}
+                      style={{ height: "24px" }}
+                    >
+                      <div className="w-[100px] text-xx text-Text-Primary">
+                        Cancel
+                      </div>
+                    </ButtonPrimary>
+                    <ButtonPrimary
+                      onClick={() => {
+                        setCommentText("");
+                        setShowAddNote(false);
+                        setData((pre: any) => ({
+                          ...pre,
+                          notes: [
+                            ...pre.notes,
+                            {
+                              date: Date.now(),
+                              time: "14:14:32.274416",
+                              writer: "",
+                              note: commentText,
+                            },
+                          ],
+                        }));
+                      }}
+                      //   onClick={() => {
+                      //     Application.addNoteHelth({
+                      //       member_id: id,
+                      //       note: commentText,
+                      //       writer: "",
+                      //     }).then(() => {
+                      //       setCommentText("");
+                      //       setShowAddNote(false);
+                      //       setData((pre: any) => ({
+                      //         ...pre,
+                      //         notes: [
+                      //           ...pre.notes,
+                      //           {
+                      //             date: Date.now(),
+                      //             time: "14:14:32.274416",
+                      //             writer: "",
+                      //             note: commentText,
+                      //           },
+                      //         ],
+                      //       }));
+                      //     });
+                      //   }}
+                      style={{ height: "24px" }}
+                    >
+                      <div className="w-[100px] text-xs">Save Note</div>
+                    </ButtonPrimary>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div
+                className="flex justify-center items-center h-[600px] overflow-y-scroll"
+                style={{
+                  alignItems: data?.notes.length > 0 ? "start" : "center",
+                }}
+              >
+                {data?.notes?.length > 0 ? (
+                  <>
+                    <div className="w-full ">
+                      {data?.notes.map((el: any) => {
+                        return (
+                          <div className="w-full px-3 my-2">
+                            <Accordion title={el.date}>
+                              <div className="text-[12px] text-justify w-full">
+                                {el.note}
+                              </div>
+                            </Accordion>
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </>
+                ) : (
+                  <div className="flex flex-col items-center justify-center">
+                    <img src="/icons/no-note.svg" alt="" />
+                    <div className="text-[12px] text-Text-Primary mt-1">
+                    No Notes Found
+                    </div>
+                  </div>
+                )}
+              </div>
+            )}
           </div>
         );
       // Add more cases as needed
