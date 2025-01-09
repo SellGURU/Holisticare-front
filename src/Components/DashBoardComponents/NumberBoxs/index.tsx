842ad30feebe1644785772ac028225c7877a77f2
amir <amirdehghani137722@gmail.com>
2025-01-09 11:20:48 +0330

    add dashboard

---
+++ src/Components/DashBoardComponents/NumberBoxs/index.tsx
@@ -0,0 +1,53 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import NumberBox from "../../NumberBox"
+interface NumberBoxesProps {
+  reports:Array<any>
+}
+
+const NumberBoxes:React.FC<NumberBoxesProps> = ({reports}) => {
+    const resolveValue =(key:string) => {
+      if(reports.length > 0){
+        return reports.filter((e) => e.key == key)[0].value
+      }
+      return 0
+    }    
+    return (
+        <>
+          <div
+            className={
+              "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-3  md:gap-4 w-full h-full"
+            }
+          >
+            <NumberBox
+              mode="added"
+              value={resolveValue("Total Enrollment")}
+              title="Total Enrollment"
+              icon={''}
+
+            />
+            <NumberBox
+              mode="increase"
+              value={resolveValue("Incomplete Client Data")}
+              title="Incomplete Client Data"
+              icon={''}
+
+            />
+            <NumberBox
+              mode="reduction"
+              value={resolveValue("Client Needs Check")}
+              title="Client Need Checking"
+              icon={''}
+            />
+            <NumberBox
+              mode="increase"
+              value={resolveValue("Client Checked")}
+              title="Clients Checked"
+              icon={''}
+
+            />
+          </div>        
+        </>
+    )
+}
+
+export default NumberBoxes
