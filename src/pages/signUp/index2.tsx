1eaf33b91a2564377445d99551c70626cb89eb64
amir <amirdehghani137722@gmail.com>
2025-01-15 11:16:02 +0330

    update register page

--- src/pages/signUp/index2.tsx
+++ src/pages/signUp/index2.tsx
@@ -1,13 +1,13 @@
-import { useFormik } from "formik";
+import { BeatLoader } from "react-spinners"
+import { ButtonSecondary } from "../../Components/Button/ButtosSecondary"
 import TextField from "../../Components/TextField"
+import AuthLayout from "../../layout/AuthLayout"
+import { useNavigate } from "react-router-dom"
+import { useFormik } from "formik"
 import * as yup from "yup";
-import { ButtonSecondary } from "../../Components/Button/ButtosSecondary";
-import Auth from "../../api/auth";
-import { useApp } from "../../hooks";
-import { useNavigate } from "react-router-dom";
-import { useState } from "react";
-import { BeatLoader} from "react-spinners";
-import AuthLayout from "../../layout/AuthLayout";
+import { useState } from "react"
+import Auth from "../../api/auth"
+import { useApp } from "../../hooks"
 
 const validationSchema = yup.object({
   email: yup.string().email("Enter a valid email").required("Email is required"),
@@ -18,55 +18,54 @@ const validationSchema = yup.object({
     .matches(/[0-9]/, "Password must contain at least one number")
     .matches(/[@$!%*?&#]/, "Password must contain at least one special character")
     .required("Password is required"),
+    userName: yup
+    .string()
+    .min(4, "Full name must be at least 4 characters")
+    .required("Full name is required"),    
 });
-
-const Login = () => {
+const SignUp = () => {
+    const navigate = useNavigate()
+    const appContext = useApp();
+    const [isLoading,setIsLoading] = useState(false)
     const formik = useFormik({
         initialValues: {
         email: "",
         password: "",
+        userName:""
         },
         validationSchema,
         validateOnChange: true,
         validateOnBlur: true,
         onSubmit:() => {}
-    });
-    const navigate = useNavigate()
-    const appContext = useApp();
-    const [isLoading,setIsLoading] = useState(false)
+    });    
     const submit = () => {
         setIsLoading(true)
-        Auth.login(formik.values.email, formik.values.password).then((res) => {
-          appContext.login(res.data.access_token,res.data.permission);
-          navigate("/");
-        }).catch((res) => {
-          if(res.detail){
-            if(res.detail == 'Account not found. Check your email again.'){
-              formik.setFieldError("email",res.detail)
-            }
-            if(res.detail == 'Incorrect password. Please try again.'){
-              formik.setFieldError("password",res.detail)
-            }        
-          }      
-        
-        }).finally(() => {
+        Auth.signup(formik.values.userName,formik.values.email,formik.values.password).then(() => {
+            Auth.login(formik.values.email,formik.values.password).then((res) => {
+                appContext.login(
+                    res.data.access_token,
+                    res.data.permission
+                );
+                navigate("/");
+            }).finally(() => {
+                setIsLoading(false)
+            })
+        }).catch(() => {
             setIsLoading(false)
-        })        
-    }    
+        })
+    }     
     return (
         <>
-            <AuthLayout>
+        <AuthLayout>
                 <div className="flex justify-center items-center mb-4">
                     <img src="./icons/HolisticareLogo.svg" alt="" />
                 </div>
-                <div className="text-xl font-medium text-Text-Primary text-center">Welcome Back!</div>
+                <div className="text-xl font-medium text-Text-Primary text-center">Create Your Account</div>
                 <div className="mt-6 grid gap-4">
+                    <TextField inValid={formik.errors?.userName != undefined && (formik.touched?.userName as boolean)}  errorMessage={formik.errors?.userName} {...formik.getFieldProps("userName")} placeholder="Enter your full name..." label="Full name" type="text" ></TextField> 
                     <TextField inValid={formik.errors?.email != undefined && (formik.touched?.email as boolean)}  errorMessage={formik.errors?.email} {...formik.getFieldProps("email")} placeholder="Enter your email address..." label="Email Address" type="email" ></TextField> 
                     <div className="mb-4">
                         <TextField errorMessage={formik.errors?.password} inValid={formik.errors?.password != undefined && (formik.touched?.password as boolean)} {...formik.getFieldProps("password")} placeholder="Enter your password..." label="Password" type="password" ></TextField> 
-                        <div className="w-full mt-2 flex justify-end items-center">
-                            <div className="text-[12px] cursor-pointer text-Primary-DeepTeal font-medium hover:opacity-85 hover:underline">Forgot password?</div>
-                        </div>
 
                     </div>
                     <ButtonSecondary ClassName="rounded-[20px]" disabled={!formik.isValid} onClick={() => {
@@ -78,22 +77,23 @@ const Login = () => {
 
                         </div>
                     :
-                    'Log in'
+                    'Sign up'
                     }
-                    </ButtonSecondary>            
+                    </ButtonSecondary>     
+                    <div className="text-[10px] text-Text-Secondary text-center mt-[-10px]">By signing up you agreed with our <span className="text-Primary-DeepTeal cursor-pointer hover:underline hover:opacity-90">Terms & Conditions.</span>  </div>       
                     <div className="flex items-center justify-center mt-4">
                         <div className="flex-grow h-px bg-gradient-to-l from-Text-Triarty via-Text-Triarty to-transparent"></div>
                         <span className="px-4 text-[14px] text-Text-Secondary">or</span>
                         <div className="flex-grow h-px bg-gradient-to-r from-Text-Triarty via-Text-Triarty to-transparent"></div>
                     </div> 
           
-                    <div className="text-[12px] text-center text-Text-Secondary">Don't have an account? <span onClick={() => {
-                        navigate('/register')
-                    }} className="text-Primary-DeepTeal font-medium hover:opacity-85 cursor-pointer hover:underline ml-[2px]">Sign up</span></div>                           
-                </div>                
-            </AuthLayout>
+                    <div className="text-[12px] text-center text-Text-Secondary">Already have an account?<span onClick={() => {
+                        navigate('/login')
+                    }} className="text-Primary-DeepTeal font-medium hover:opacity-85 cursor-pointer hover:underline ml-[2px]">Log in</span></div>                           
+                </div>    
+        </AuthLayout>
         </>
     )
 }
 
-export default Login
+export default SignUp
