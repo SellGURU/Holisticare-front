da76142b32ab469517858a481f2b997bd8a68956
amir <amirdehghani137722@gmail.com>
2025-01-19 15:39:51 +0330

    update client

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -7,7 +7,7 @@ const Home = () => {
       <div className="w-full sticky z-50 top-0 ">
         <MainTopBar></MainTopBar>
       </div>
-      <div className="fixed left-0 top-0 z-[10]">
+      <div className="fixed left-0 top-0 z-[60]">
         <SideMenu></SideMenu>
       </div>
       <div className="w-full pl-[84px] pt-0 pb-2 h-[100vh] overflow-y-scroll">
