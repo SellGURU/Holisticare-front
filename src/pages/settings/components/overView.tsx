b355b0a26fd00e0dbb82ee3f12f06251d9c64de1
amir <amirdehghani137722@gmail.com>
2025-01-21 15:22:55 +0330

    fix format

--- src/pages/settings/components/overView.tsx
+++ src/pages/settings/components/overView.tsx
@@ -67,40 +67,66 @@ export const OverView = () => {
         </div>
       </div>
       <div className="bg-backgroundColor-Card rounded-2xl p-4 shadow-100 text-Text-Primary mt-8">
-      <div className="flex justify-between items-center">
-        <h2 className="text-2xl mb-6">Billing and subscription</h2>
-        <span className="text-xs text-Text-Secondary">Last update: 2024/02/02</span>
-      </div>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-        <div className='pr-6'>
-          <h3 className="text-sm font-medium mb-3">Current Plan</h3>
-          <p className="text-sm text-Text-Secondary mb-2">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.
-          </p>
-          <div style={{ background: 'linear-gradient(to right, #005F73 25%, #6CC24A 100%)' }}  className="mt-4 rounded-lg p-4 text-white relative text-center overflow-hidden">
-            <img className='absolute inset-0 w-full' src="/images/billingBg.svg" alt="" />
-            <p className="text-lg text-white">Pro</p>
-            <p className="text-[10px] text-white">Since September 27th, 2024</p>
+        <div className="flex justify-between items-center">
+          <h2 className="text-2xl mb-6">Billing and subscription</h2>
+          <span className="text-xs text-Text-Secondary">
+            Last update: 2024/02/02
+          </span>
+        </div>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+          <div className="pr-6">
+            <h3 className="text-sm font-medium mb-3">Current Plan</h3>
+            <p className="text-sm text-Text-Secondary mb-2">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore.
+            </p>
+            <div
+              style={{
+                background:
+                  'linear-gradient(to right, #005F73 25%, #6CC24A 100%)',
+              }}
+              className="mt-4 rounded-lg p-4 text-white relative text-center overflow-hidden"
+            >
+              <img
+                className="absolute inset-0 w-full"
+                src="/images/billingBg.svg"
+                alt=""
+              />
+              <p className="text-lg text-white">Pro</p>
+              <p className="text-[10px] text-white">
+                Since September 27th, 2024
+              </p>
+            </div>
+          </div>
+          <div className="border-l border-Boarder pl-10">
+            <h3 className="text-sm font-meidum">Manage Subscription</h3>
+            <p className="text-sm text-Text-Secondary mt-3">
+              You have 23 days left in your package. If you cancel the
+              subscription now, no refund will be issued but you can still use
+              the remaining days.
+            </p>
+            <div className="mt-4 w-full flex justify-center gap-8 ">
+              <button className="text-Primary-DeepTeal text-xs font-medium">
+                Downgrade Subscription
+              </button>
+
+              <ButtonSecondary>Upgrade Subscription</ButtonSecondary>
+            </div>
+            <div className=" mt-3 w-full flex justify-center">
+              <ButtonSecondary
+                ClassName="shadow-100"
+                style={{
+                  backgroundColor: '#FDFDFD',
+                  color: '#005F73',
+                  width: '100%',
+                }}
+              >
+                Upgrade Subscription
+              </ButtonSecondary>
+            </div>
           </div>
         </div>
-        <div className="border-l border-Boarder pl-10">
-          <h3 className="text-sm font-meidum">Manage Subscription</h3>
-          <p className="text-sm text-Text-Secondary mt-3">
-            You have 23 days left in your package. If you cancel the subscription now, no refund will be issued but you can still use the remaining days.
-          </p>
-          <div className="mt-4 w-full flex justify-center gap-8 ">
-            <button className="text-Primary-DeepTeal text-xs font-medium">Downgrade Subscription</button>
-           
-            <ButtonSecondary>Upgrade Subscription</ButtonSecondary>
-           
-          </div>
-          <div className=' mt-3 w-full flex justify-center'>
-          <ButtonSecondary ClassName='shadow-100' style={{backgroundColor: '#FDFDFD' , color:'#005F73' , width:'100%'}}>Upgrade Subscription</ButtonSecondary>
-          </div>
-        
-        </div>
       </div>
-    </div>
     </>
   );
 };
