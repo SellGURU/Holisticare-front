522a2cd77837b2930b5d3f83b098c8bd7a05633e
Amirhossein Rahmani <amirrahmani2006@gmail.com>
2025-01-27 15:27:26 +0330

    refactor message structure and update related components

--- src/pages/messages/index.tsx
+++ src/pages/messages/index.tsx
@@ -6,7 +6,7 @@ import {
 export const Messages = () => {
   return (
     <div className="w-full h-full flex justify-between px-6 py-10 gap-5">
-      <div className="w-[315px] h-full">
+      <div className="w-[315px] h-[90%]">
         <MessageList isMessages></MessageList>
       </div>
       <MessagesChatBox></MessagesChatBox>
