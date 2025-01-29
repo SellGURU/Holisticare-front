95a82b2b692ab9410e85032f4df998dbe93244a3
SmurfsNIma <sadeghinima090@gmail.com>
2025-01-29 09:15:54 +0330

    drfit fix link navigate

--- src/pages/driftAnaysis/ClientCard.tsx
+++ src/pages/driftAnaysis/ClientCard.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { Dispatch, SetStateAction } from 'react';
 import SvgIcon from '../../utils/svgIcon';
+import { useNavigate } from 'react-router-dom';
 
 interface ClientCardProps {
   index: number;
@@ -26,6 +27,7 @@ export const ClientCard: React.FC<ClientCardProps> = ({
   setCardActive,
   isSwitch,
 }) => {
+  const navigate = useNavigate()
   return (
     <div
       onClick={() => {
@@ -84,7 +86,7 @@ export const ClientCard: React.FC<ClientCardProps> = ({
       <div
         className={` ${isSwitch && 'hidden'} absolute right-1 bottom-1 flex flex-col gap-4`}
       >
-        <div className="cursor-pointer bg-white border border-Gray-50 shadow-100 w-8 h-8   rounded-full p-2">
+        <div onClick={()=>navigate(`/report/${memberID}/${name}`)} className="cursor-pointer bg-white border border-Gray-50 shadow-100 w-8 h-8   rounded-full p-2">
           {' '}
           <SvgIcon src='/icons/export.svg' color='#005F73' />
           {/* <img src="/icons/export.svg" alt="" /> */}
