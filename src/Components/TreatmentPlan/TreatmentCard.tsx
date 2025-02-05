293d41150b5f3979f461ea2ec5a5ce78649a7532
SmurfsNIma <sadeghinima090@gmail.com>
2025-02-05 15:43:02 +0330

    holistic responsive issue

--- src/Components/TreatmentPlan/TreatmentCard.tsx
+++ src/Components/TreatmentPlan/TreatmentCard.tsx
@@ -4,14 +4,14 @@ interface TreatmentCardProps {
 }
 const TreatmentCard: React.FC<TreatmentCardProps> = ({ data }) => {
   return (
-    <div className="w-[354px] no-split print:w-full h-[200px] print:h-auto flex items-center px-4 py-2 bg-backgroundColor-Card border border-Gray-50 rounded-2xl relative ">
-      <div className="text-sm text-Text-Primary absolute top-3">
+    <div className="w-[354px] no-split print:w-full h-full md:h-[200px] print:h-auto flex flex-col gap-4 px-4 py-2 bg-backgroundColor-Card border border-Gray-50 rounded-2xl relative ">
+      <div className="text-sm text-Text-Primary ">
         {data.title}
       </div>
-      <div className=" text-Text-Secondary text-xs absolute top-16 ">
+      <div className=" text-Text-Secondary text-xs ">
         <span className="text-Text-Primary text-xs">Notes:</span> {data.Notes}
       </div>
-      <div className="text-xs font-medium text-Primary-DeepTeal absolute top-[150px] select-none">
+      <div className="text-xs font-medium text-Primary-DeepTeal select-none">
         {' '}
         {data.Based}
       </div>
