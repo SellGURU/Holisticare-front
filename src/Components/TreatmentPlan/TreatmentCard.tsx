3d2b57e6cda46e27454d56cce991733fc4fb5517
amir <amirdehghani137722@gmail.com>
2025-02-05 16:03:01 +0330

    format treatment plan

--- src/Components/TreatmentPlan/TreatmentCard.tsx
+++ src/Components/TreatmentPlan/TreatmentCard.tsx
@@ -5,9 +5,7 @@ interface TreatmentCardProps {
 const TreatmentCard: React.FC<TreatmentCardProps> = ({ data }) => {
   return (
     <div className="w-[354px] no-split print:w-full h-full md:h-[200px] print:h-auto flex flex-col gap-4 px-4 py-2 bg-backgroundColor-Card border border-Gray-50 rounded-2xl relative ">
-      <div className="text-sm text-Text-Primary ">
-        {data.title}
-      </div>
+      <div className="text-sm text-Text-Primary ">{data.title}</div>
       <div className=" text-Text-Secondary text-xs ">
         <span className="text-Text-Primary text-xs">Notes:</span> {data.Notes}
       </div>
