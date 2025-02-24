41ff716226c26f68236e69da0924a6d17ac7aa00
amir <amirdehghani137722@gmail.com>
2025-02-24 16:28:50 +0330

    format client list

--- src/Components/Toggle/index.tsx
+++ src/Components/Toggle/index.tsx
@@ -2,18 +2,24 @@ interface ToggleProps {
   value: Array<string>;
   active: string;
   setActive: (value: string) => void;
-  isClientList?: boolean,
+  isClientList?: boolean;
 }
-const Toggle: React.FC<ToggleProps> = ({ value, active, setActive,isClientList }) => {
+const Toggle: React.FC<ToggleProps> = ({
+  value,
+  active,
+  setActive,
+  isClientList,
+}) => {
   return (
     <>
-      <div className={` bg-backgroundColor-Main ${isClientList ? "h-8 w-[324px]" : " h-7 w-[294px] " }  px-2 flex justify-between items-center rounded-[24px] gap-1`}>
-
+      <div
+        className={` bg-backgroundColor-Main ${isClientList ? 'h-8 w-[324px]' : ' h-7 w-[294px] '}  px-2 flex justify-between items-center rounded-[24px] gap-1`}
+      >
         <div
           onClick={() => {
             setActive(value[0]);
           }}
-          className={` cursor-pointer ${active == value[0] ? 'bg-Primary-DeepTeal text-white' : '  bg-Secondary-SelverGray border text-Text-Primary'}  ${isClientList ? "h-[24px] w-[100px]" : " h-[20px] w-[135px]" } rounded-[16px] flex justify-center items-center text-[12px]`}
+          className={` cursor-pointer ${active == value[0] ? 'bg-Primary-DeepTeal text-white' : '  bg-Secondary-SelverGray border text-Text-Primary'}  ${isClientList ? 'h-[24px] w-[100px]' : ' h-[20px] w-[135px]'} rounded-[16px] flex justify-center items-center text-[12px]`}
         >
           {value[0]}
         </div>
@@ -22,7 +28,7 @@ const Toggle: React.FC<ToggleProps> = ({ value, active, setActive,isClientList }
           onClick={() => {
             setActive(value[1]);
           }}
-          className={` cursor-pointer ${active == value[1] ? ' bg-Primary-DeepTeal text-white' : '  bg-Secondary-SelverGray border text-Text-Primary'}   ${isClientList ? "h-[24px] w-[100px]" : " h-[20px] w-[135px]" } rounded-[16px] flex justify-center items-center text-[12px]`}
+          className={` cursor-pointer ${active == value[1] ? ' bg-Primary-DeepTeal text-white' : '  bg-Secondary-SelverGray border text-Text-Primary'}   ${isClientList ? 'h-[24px] w-[100px]' : ' h-[20px] w-[135px]'} rounded-[16px] flex justify-center items-center text-[12px]`}
         >
           {value[1]}
         </div>
@@ -33,7 +39,7 @@ const Toggle: React.FC<ToggleProps> = ({ value, active, setActive,isClientList }
               onClick={() => {
                 setActive(value[2]);
               }}
-              className={` cursor-pointer ${active == value[2] ? ' bg-Primary-DeepTeal text-white' : '  bg-Secondary-SelverGray border text-Text-Primary'}  ${isClientList ? "h-[24px] w-[100px]" : " h-[20px] w-[135px]" } rounded-[16px] flex justify-center items-center text-[12px]`}
+              className={` cursor-pointer ${active == value[2] ? ' bg-Primary-DeepTeal text-white' : '  bg-Secondary-SelverGray border text-Text-Primary'}  ${isClientList ? 'h-[24px] w-[100px]' : ' h-[20px] w-[135px]'} rounded-[16px] flex justify-center items-center text-[12px]`}
             >
               {value[2]}
             </div>
