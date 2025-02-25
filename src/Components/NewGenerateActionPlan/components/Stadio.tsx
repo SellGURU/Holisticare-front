4a49493e4b580dee29ed512bc9187a139448a6d8
amir <amirdehghani137722@gmail.com>
2025-02-25 10:22:14 +0330

    format files

--- src/Components/NewGenerateActionPlan/components/Stadio.tsx
+++ src/Components/NewGenerateActionPlan/components/Stadio.tsx
@@ -76,62 +76,61 @@ const Stadio: React.FC<StadioProps> = ({
     <>
       <div className="flex px-6 gap-4">
         <div className="flex-grow">
-            {/* alert */}
-            {haveConflic && (
-              <div className="text-[12px]">
-                Aww yeah, you successfully read this important alert message.
-                This example text is going to run a bit longer so that you can
-                see how spacing within an alert works with this kind of content.
-              </div>
-            )}
-            <div
-              className={`w-full bg-white rounded-[24px] border border-gray-50 shadow-100 sticky  top-[190px] h-[480px]  ${actions.length != 0 && ''} `}
-            >
-              {actions.length == 0 ? (
-                <div className="flex flex-col items-center justify-center w-full h-[500px]">
-                  <img
-                    src="/icons/document-text.svg"
-                    alt=""
-                    className="w-[87px] h-[87px]"
-                  />
-                  <div className="text-Text-Primary font-medium text-base mt-2">
-                    No action to show
-                  </div>
-                  <ButtonSecondary
-                    ClassName="rounded-[20px] mt-8"
-                    onClick={() => {
-                      AutoGenerate();
-                    }}
-                  >
-                    {isAutoGenerate ? (
-                      <SpinnerLoader />
-                    ) : (
-                      <>
-                        <img
-                          src="/icons/tree-start-white.svg"
-                          alt=""
-                          className="mr-2"
-                        />
-                        Auto Generate
-                      </>
-                    )}
-                  </ButtonSecondary>
-                </div>
-              ) : (
-                <>
-                  <div className="max-h-[480px] grid grid-cols-1 gap-3 py-3 overflow-y-auto">
-                    {actions.map((act: any) => {
-                      return (
-                        <>
-                          <ActionCard data={act}></ActionCard>
-                        </>
-                      );
-                    })}
-                  </div>
-                </>
-              )}
+          {/* alert */}
+          {haveConflic && (
+            <div className="text-[12px]">
+              Aww yeah, you successfully read this important alert message. This
+              example text is going to run a bit longer so that you can see how
+              spacing within an alert works with this kind of content.
             </div>
-
+          )}
+          <div
+            className={`w-full bg-white rounded-[24px] border border-gray-50 shadow-100 sticky  top-[190px] h-[480px]  ${actions.length != 0 && ''} `}
+          >
+            {actions.length == 0 ? (
+              <div className="flex flex-col items-center justify-center w-full h-[500px]">
+                <img
+                  src="/icons/document-text.svg"
+                  alt=""
+                  className="w-[87px] h-[87px]"
+                />
+                <div className="text-Text-Primary font-medium text-base mt-2">
+                  No action to show
+                </div>
+                <ButtonSecondary
+                  ClassName="rounded-[20px] mt-8"
+                  onClick={() => {
+                    AutoGenerate();
+                  }}
+                >
+                  {isAutoGenerate ? (
+                    <SpinnerLoader />
+                  ) : (
+                    <>
+                      <img
+                        src="/icons/tree-start-white.svg"
+                        alt=""
+                        className="mr-2"
+                      />
+                      Auto Generate
+                    </>
+                  )}
+                </ButtonSecondary>
+              </div>
+            ) : (
+              <>
+                <div className="max-h-[480px] grid grid-cols-1 gap-3 py-3 overflow-y-auto">
+                  {actions.map((act: any) => {
+                    return (
+                      <>
+                        <ActionCard data={act}></ActionCard>
+                      </>
+                    );
+                  })}
+                </div>
+              </>
+            )}
+          </div>
         </div>
         <div className="w-[342px] p-4 min-h-[200px] bg-white rounded-[24px] border border-gray-50 shadow-100">
           <SearchBox
