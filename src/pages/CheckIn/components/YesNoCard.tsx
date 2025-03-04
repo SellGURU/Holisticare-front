c8289b37b4e6fc7811c7712c70e2b065b509c5f8
amir <amirdehghani137722@gmail.com>
2025-03-04 10:01:23 +0330

    format files

--- src/pages/CheckIn/components/YesNoCard.tsx
+++ src/pages/CheckIn/components/YesNoCard.tsx
@@ -1,35 +1,39 @@
-import { useState } from "react"
+import { useState } from 'react';
 
 interface YesNoCardProps {
-    question:string
-    value:string
+  question: string;
+  value: string;
 }
 
-const YesNoCard:React.FC<YesNoCardProps> = ({question,value}) => {
-    const [val,setVal] = useState(value)
-    return (
-        <>
-            <div className="bg-[#FCFCFC] p-3 w-full h-[92px] rounded-[12px] border border-gray-50">
-            <div className="text-[12px] text-Text-Primary">
-                {question}
+const YesNoCard: React.FC<YesNoCardProps> = ({ question, value }) => {
+  const [val, setVal] = useState(value);
+  return (
+    <>
+      <div className="bg-[#FCFCFC] p-3 w-full h-[92px] rounded-[12px] border border-gray-50">
+        <div className="text-[12px] text-Text-Primary">{question}</div>
+        <div className="flex justify-end items-center mt-4">
+          <div className="w-[96px] cursor-pointer flex justify-between items-center h-[32px] border border-gray-50 rounded-[8px]">
+            <div
+              onClick={() => {
+                setVal('Yes');
+              }}
+              className={`w-[50%] text-[10px] ${val == 'Yes' ? 'bg-Primary-EmeraldGreen rounded-l-[8px] text-white' : ''} h-full  flex justify-center items-center`}
+            >
+              Yes
             </div>
-            <div className="flex justify-end items-center mt-4">
-                <div className="w-[96px] cursor-pointer flex justify-between items-center h-[32px] border border-gray-50 rounded-[8px]">
-                <div onClick={() => {
-                    setVal("Yes")
-                }} className={`w-[50%] text-[10px] ${val == 'Yes' ?'bg-Primary-EmeraldGreen rounded-l-[8px] text-white':''} h-full  flex justify-center items-center`}>
-                    Yes
-                </div>
-                <div onClick={() => {
-                    setVal("No")
-                }} className={`w-[50%] text-[10px] ${val == 'No' ?'bg-Primary-EmeraldGreen rounded-r-[8px] text-white':''}   h-full  flex justify-center items-center`}>
-                    No
-                </div>
-                </div>
+            <div
+              onClick={() => {
+                setVal('No');
+              }}
+              className={`w-[50%] text-[10px] ${val == 'No' ? 'bg-Primary-EmeraldGreen rounded-r-[8px] text-white' : ''}   h-full  flex justify-center items-center`}
+            >
+              No
             </div>
-            </div>        
-        </>
-    )
-}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default YesNoCard
+export default YesNoCard;
--- src/pages/CheckIn/index.tsx
+++ src/pages/CheckIn/index.tsx
@@ -1,11 +1,13 @@
-import YesNoCard from "./components/YesNoCard";
+import YesNoCard from './components/YesNoCard';
 
 const Checkin = () => {
   return (
     <>
       <div className=" px-6 py-4">
-        <YesNoCard value="Yes" question="1-Did you stick to the Meal Plan?"></YesNoCard>
-
+        <YesNoCard
+          value="Yes"
+          question="1-Did you stick to the Meal Plan?"
+        ></YesNoCard>
       </div>
     </>
   );
