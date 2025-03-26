fa685bc31ebdf5e75a798108f97fc84887d878a1
amir <amirdehghani137722@gmail.com>
2025-03-27 00:27:33 +0330

    Enhance Exercise components to support exercise index tracking and update onChange handlers. Modify ExerciseItem and SuperSetExerciseItem to pass exercise index for better state management in Superset functionality.

--- src/pages/Library/Activity/AddComponents/SuperSetExersiseItem.tsx
+++ src/pages/Library/Activity/AddComponents/SuperSetExersiseItem.tsx
@@ -6,7 +6,7 @@ interface SuperSetExersiseItemProps {
   exercise: any;
   onDelete: () => void;
   toSuperSet: () => void;
-  onChange: (index: number, field: string, value: string) => void;
+  onChange: (index: number, field: string, value: string, exersiseIndex: number) => void;
 }
 
 const SuperSetExersiseItem: React.FC<SuperSetExersiseItemProps> = ({
@@ -19,8 +19,10 @@ const SuperSetExersiseItem: React.FC<SuperSetExersiseItemProps> = ({
   return (
     <>
       <div className="flex flex-col ml-2 gap-2 relative">
-        {exercise.Exercises.map((el: any) => (
+        {exercise.Exercises.map((el: any,ind:number) => (
           <ExerciseItem
+            sets={exercise.Sets}
+            exesiseIndex={ind}
             onDelete={onDelete}
             key={index}
             index={index}
