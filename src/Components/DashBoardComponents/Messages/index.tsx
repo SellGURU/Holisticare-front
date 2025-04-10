d06863a30cd5a54fcbb5e84afe298648542f8b04
amir <amirdehghani137722@gmail.com>
2025-04-10 16:40:34 +0330

    Update MessageList component to improve message display with text truncation and styling adjustments; comment out unused SearchBox in Package component for cleaner code.

--- src/Components/DashBoardComponents/Messages/index.tsx
+++ src/Components/DashBoardComponents/Messages/index.tsx
@@ -233,10 +233,10 @@ const MessageList: React.FC<MessageListProps> = ({ isMessages }) => {
                         )} */}
                       </div>
                       <div
-                        className={`text-[10px] text-Text-Secondary   ${
+                        className={`text-[10px] text-nowrap  overflow-ellipsis overflow-hidden w-[120px] max-w-[150px] text-Text-Secondary   ${
                           expandedMessage === message.member_id
                             ? ''
-                            : 'line-clamp-2'
+                            : ''
                         } `}
                       >
                         {message.message}
