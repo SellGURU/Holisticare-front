f7a2de801195674695567affc771a39c02c45ee6
amir <amirdehghani137722@gmail.com>
2025-04-13 14:26:17 +0330

    Update API base URLs in Api and ApiMobile classes to point to test endpoints for development purposes.

--- src/api/apiMobile.ts
+++ src/api/apiMobile.ts
@@ -3,6 +3,7 @@ import Api from './api';
 class ApiMobile extends Api {
   protected static base_url: string =
     'https://vercel-backend-one-roan.vercel.app/holisticare/mobile';
+    // 'https://vercel-backend-one-roan.vercel.app/holisticare_test/mobile';
 }
 
 export default ApiMobile;
