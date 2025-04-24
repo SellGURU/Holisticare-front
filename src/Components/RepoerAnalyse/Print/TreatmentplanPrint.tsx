3114ed0a198b391f50dbbc8cfad2cf56ae311d52
amir <amirdehghani137722@gmail.com>
2025-04-24 09:48:45 +0330

    Refactor TreatmentPlanPrint component to enhance conditional rendering of key benefits and risks, improving code readability and presentation.

--- src/Components/RepoerAnalyse/Print/TreatmentplanPrint.tsx
+++ src/Components/RepoerAnalyse/Print/TreatmentplanPrint.tsx
@@ -30,24 +30,30 @@ const TreatmentPlanPrint: React.FC<TreatmentPlanPrintProps> = ({ data }) => {
           >
             {data.title}
           </div>
-          <div
-            className="my-2 flex"
-            style={{ fontSize: '12px', color: '#383838' }}
-          >
-            <div style={{ marginRight: '5px', color: '#888888' }}>&#8226;</div>
-            <div>
-              <span style={{ color: '#888888' }}>Key Benefits</span> {positive}
+          {positive &&
+            <div
+              className="my-2 flex"
+              style={{ fontSize: '12px', color: '#383838' }}
+            >
+              <div style={{ marginRight: '5px', color: '#888888' }}>&#8226;</div>
+              <div>
+                <span style={{ color: '#888888' }}>Key Benefits</span> {positive}
+              </div>
             </div>
-          </div>
-          <div
-            className="my-2 flex"
-            style={{ fontSize: '12px', color: '#383838' }}
-          >
-            <div style={{ marginRight: '5px', color: '#888888' }}>&#8226;</div>
-            <div>
-              <span style={{ color: '#888888' }}>Key Risks</span> {negative}
+          }
+          {
+            negative &&
+            <div
+              className="my-2 flex"
+              style={{ fontSize: '12px', color: '#383838' }}
+            >
+              <div style={{ marginRight: '5px', color: '#888888' }}>&#8226;</div>
+              <div>
+                <span style={{ color: '#888888' }}>Key Risks</span> {negative}
+              </div>
             </div>
-          </div>
+
+          }
           {data?.Client_Notes?.length > 0 && (
             <>
               <div
