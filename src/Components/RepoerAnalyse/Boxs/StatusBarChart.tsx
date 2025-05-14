0b651e610cf2ed480823952efcf1354095df64dd
amir <amirdehghani137722@gmail.com>
2025-05-14 15:18:13 +0330

    Refactor condition in StatusBarChart component for improved logic clarity

--- src/Components/RepoerAnalyse/Boxs/StatusBarChart.tsx
+++ src/Components/RepoerAnalyse/Boxs/StatusBarChart.tsx
@@ -117,8 +117,7 @@ const StatusBarChart: React.FC<StatusBarChartProps> = ({ data, justView }) => {
                   </>
                 ) : (
                   <>
-                    {(data.chart_bounds[el.key].label.toLowerCase() ==
-                      data.values[0].toLowerCase() ||
+                    {(
                       // el.value[0]?.toString().includes(data.values[0]) ||
                       data.status[0] == el.key) &&
                       !justView && (
