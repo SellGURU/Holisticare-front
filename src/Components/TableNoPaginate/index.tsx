18085fdb571c7efb006cb04f9294c4aed9f2796e
SmurfsNima <sadeghinima090@gmail.com>
2025-05-25 15:33:26 +0330

    fix package

--- src/Components/TableNoPaginate/index.tsx
+++ src/Components/TableNoPaginate/index.tsx
@@ -12,6 +12,7 @@ import { FaSort } from 'react-icons/fa';
 
 interface TableProps {
   classData: Array<any>;
+  isPackage?: boolean;  
 }
 // Custom filter function to handle nested fields
 const nestedFilter: FilterFn<any> = (row, columnId, filterValue) => {
@@ -24,8 +25,9 @@ const nestedFilter: FilterFn<any> = (row, columnId, filterValue) => {
   return String(rowValue).toLowerCase().includes(filterValue.toLowerCase());
 };
 
-const TableNoPaginate: FC<TableProps> = ({ classData }) => {
+const TableNoPaginate: FC<TableProps> = ({ classData, isPackage }) => {
   const [data, setData] = useState(classData);
+
   useEffect(() => {
     setData(classData);
   }, [classData]);
@@ -40,22 +42,22 @@ const TableNoPaginate: FC<TableProps> = ({ classData }) => {
     <div className="flex items-center justify-center">
       <div className="w-full mt-4">
         <div
-          className={`flex flex-col justify-between overflow-x-auto bg-white rounded-[16px] text-Text-Primary mt-[-12px] p-2 border border-Boarder`}
+          className={`flex flex-col justify-between overflow-x-auto  rounded-[16px] text-Text-Primary mt-[-12px]  shadow-200 border border-Boarder`}
         >
           {table.getRowModel().rows.length > 0 ? (
             <table
               className={`border-collapse table-auto text-sm text-left rtl:text-right w-full`}
             >
-              <thead className="text-xs text-Text-Primary bg-bg-color">
+              <thead className={`text-xs text-Text-Primary ${isPackage ? 'bg-[#F4F4F4]' : 'bg-bg-color'} `}>
                 {table.getHeaderGroups().map((headerGroup) => (
                   <tr
                     key={headerGroup.id}
-                    className="text-nowrap text-Primary-DeepTeal"
+                    className={`text-nowrap ${isPackage ? 'text-Text-Primary': 'text-Primary-DeepTeal'}`}
                   >
                     {headerGroup.headers.map((header, index) => (
                       <th
                         key={header.id}
-                        className={`px-3 pt-3 pb-3 text-xs font-medium cursor-pointer first:rounded-tl-[12px] first:rounded-bl-[12px] last:rounded-tr-[12px] last:rounded-br-[12px]`}
+                        className={`px-3 pt-3 pb-3 text-xs font-medium cursor-pointer `}
                       >
                         <div
                           className={`flex items-center  ${index == 0 ? 'justify-start ' : 'justify-center '} `}
@@ -68,12 +70,12 @@ const TableNoPaginate: FC<TableProps> = ({ classData }) => {
                               header.column.columnDef.header,
                               header.getContext(),
                             )}
-                            {header.column.getCanSort() &&
+                            {!isPackage && header.column.getCanSort() &&
                               header.column.getIsSorted() === false && (
                                 <FaSort className="cursor-pointer" />
                               )}
-                            {header.column.getIsSorted() === 'asc' && ' 🔼'}
-                            {header.column.getIsSorted() === 'desc' && ' 🔽'}
+                            {!isPackage && header.column.getIsSorted() === 'asc' && ' 🔼'}
+                            {!isPackage && header.column.getIsSorted() === 'desc' && ' 🔽'}
                           </div>
                         </div>
                       </th>
@@ -82,9 +84,9 @@ const TableNoPaginate: FC<TableProps> = ({ classData }) => {
                 ))}
               </thead>
               <tbody>
-                {table.getRowModel().rows.map((row) => (
+                {table.getRowModel().rows.map((row,index) => (
                   <tr
-                    className={`text-Text-Primary space-y-7 bg-white`}
+                    className={`text-Text-Primary space-y-7 ${index % 2 === 0 ? 'bg-white' : 'bg-[#F4F4F4]'}`}
                     key={row.id}
                   >
                     {row.getVisibleCells().map((cell) => (
