6d04040d42e51fe802a25993521f6a240f015e4d
amir <amirdehghani137722@gmail.com>
2025-06-07 19:12:10 +0330

    Update SearchBox and ClientList components: increase icon sizes for improved visibility and user experience.

--- src/Components/clientList/index.tsx
+++ src/Components/clientList/index.tsx
@@ -380,7 +380,9 @@ const ClientList = () => {
                         }  w-full flex items-center justify-center rounded-md rounded-r-none cursor-pointer sm:min-w-12`}
                       >
                         <SvgIcon
-                          src="/icons/grid-1.svg"
+                          src="/icons/grid-1.svg" 
+                          width="20px"
+                          height="20px"
                           color={activeList == 'grid' ? '#FFF' : '#38383899'}
                         />
                       </div>
@@ -394,6 +396,8 @@ const ClientList = () => {
                       >
                         <SvgIcon
                           src="/icons/textalign-left.svg"
+                          width="20px"
+                          height="20px"
                           color={activeList == 'list' ? '#FFF' : '#38383899'}
                         />
                       </div>
