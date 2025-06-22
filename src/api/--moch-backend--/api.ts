a0729bace46788440ec67a3774d1c315524ba0d6
amir <amirdehghani137722@gmail.com>
2025-06-22 15:16:10 +0330

    Refactor API and mock data formatting: Standardize quotes in api.ts and useMoch.ts, and improve JSON structure in login.json and patients.json for better readability.

--- src/api/--moch-backend--/api.ts
+++ src/api/--moch-backend--/api.ts
@@ -1,19 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import axios from "axios";
-import MockAdapter from "axios-mock-adapter";
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
 
 const mock = new MockAdapter(axios, { delayResponse: 2000 });
 
 class Api {
-  protected static base_url: string =
-    "https://mock.app";
+  protected static base_url: string = 'https://mock.app';
 
   public static post(url: string, reply: any, body?: any) {
     const response = mock.onPost(this.base_url + url, body).reply(200, reply);
     return response;
   }
-  public static get(url: string ,reply: any) {
-    const response = mock.onGet(this.base_url + url).reply(200,reply)
+  public static get(url: string, reply: any) {
+    const response = mock.onGet(this.base_url + url).reply(200, reply);
     return response;
   }
 }
--- src/api/--moch-backend--/useMoch.ts
+++ src/api/--moch-backend--/useMoch.ts
@@ -1,8 +1,7 @@
-import Api from "./api";
+import Api from './api';
 import patients from './data/patients.json';
 const useMoch = () => {
-    Api.get("/patients",patients)     
+  Api.get('/patients', patients);
 };
 
-
-export default useMoch
+export default useMoch;
