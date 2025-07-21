24ea41ebe4c98f772cee231ba3d329fdf128fd22
Amirhossein Rahmani <amirrahmani2006@gmail.com>
2025-07-21 17:06:11 +0330

    Refactor file deletion handling and add DeletedTooltip component for improved user feedback during file deletion processes.

--- src/Components/ComboBar/components/FileHistoryNew.tsx
+++ src/Components/ComboBar/components/FileHistoryNew.tsx
@@ -264,7 +264,7 @@ const FileHistoryNew = () => {
   const [isDeleted, setIsDeleted] = useState<string[]>([]);
   const handleDelete = (fileId: string, memberId: string) => {
     setLoadingDelete(true);
-    publish('fileIsDeleted', { isDeleting: false });
+    publish('fileIsDeleting', { isDeleting: false });
     Application.deleteFileHistory({
       file_id: fileId,
       member_id: memberId,
@@ -273,6 +273,9 @@ const FileHistoryNew = () => {
         if (res.data) {
           setLoadingDelete(false);
           setIsDeleted((prev) => [...prev, fileId]);
+          publish('fileIsDeleting', {
+            isDeleting: true,
+          });
         }
       })
       .finally(() => {
