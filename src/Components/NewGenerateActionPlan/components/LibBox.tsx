bf9bb9024c65308ef564ccdc3bc0b470480dfa50
Amirhossein Rahmani <amirrahmani2006@gmail.com>
2025-07-23 11:33:57 +0330

    Updated background color logic in LibBox component to improve readability
    - Removed CSS classes related to background color and used inline styles to specify colors.
    - This change helps improve consistency and visual clarity in color representation.

--- src/Components/NewGenerateActionPlan/components/LibBox.tsx
+++ src/Components/NewGenerateActionPlan/components/LibBox.tsx
@@ -115,10 +115,12 @@ const LibBox: FC<LibBoxProps> = ({
           >
             <div className={`flex items-center gap-1`}>
               <div
-                className={`bg-[${bgColor}] select-none rounded-full px-2 py-[2px] flex items-center gap-1 text-[8px] text-Text-Primary`}
+                className={`select-none rounded-full px-2 py-[2px] flex items-center gap-1 text-[8px] text-Text-Primary`}
+                style={{ backgroundColor: bgColor }}
               >
                 <div
-                  className={`size-[8px] select-none bg-[${color}] rounded-full`}
+                  className={`size-[8px] select-none rounded-full`}
+                  style={{ backgroundColor: color }}
                 ></div>
                 {data?.label || '-'}
               </div>
