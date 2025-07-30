4875c8202d20b27deb6e157ff668e864cf348758
Amirhossein Rahmani <amirrahmani2006@gmail.com>
2025-07-30 20:23:50 +0330

    Updated text length in TableNoPaginate component to better display notes and updated Tooltip IDs to avoid conflicts. Text length was limited to 25 characters and minor changes were made to Tooltip display.

--- src/Components/LibraryThreePages/components/TableNoPaginate/tableTd.tsx
+++ src/Components/LibraryThreePages/components/TableNoPaginate/tableTd.tsx
@@ -138,7 +138,7 @@ export const columns = (pageType: string): ColumnDef<any>[] => [
       return (
         <>
           <div
-            data-tooltip-id={`tooltip-${row.original?.Ai_note?.substring(0, 47)}-c`}
+            data-tooltip-id={`tooltip-${row.original?.Ai_note?.substring(0, 25)}-c`}
             className="overflow-hidden select-none text-xs text-Text-Quadruple cursor-default"
             style={{
               textWrap: 'nowrap',
@@ -147,14 +147,14 @@ export const columns = (pageType: string): ColumnDef<any>[] => [
             }}
           >
             {row.original?.Ai_note
-              ? row.original?.Ai_note?.length > 47
-                ? row.original?.Ai_note?.substring(0, 47) + '...'
+              ? row.original?.Ai_note?.length > 25
+                ? row.original?.Ai_note?.substring(0, 25) + '...'
                 : row.original?.Ai_note
               : '-'}
           </div>
-          {row.original?.Ai_note?.length > 47 && (
+          {row.original?.Ai_note?.length > 25 && (
             <Tooltip
-              id={`tooltip-${row.original?.Ai_note?.substring(0, 47)}-c`}
+              id={`tooltip-${row.original?.Ai_note?.substring(0, 25)}-c`}
               place="top"
               className="!bg-white !bg-opacity-100 !opacity-100 !max-w-[250px] !leading-5 !text-wrap !shadow-100 !text-[#888888] !text-[10px] !rounded-[6px] !border !border-Gray-50 flex flex-col !z-[99999]"
             >
