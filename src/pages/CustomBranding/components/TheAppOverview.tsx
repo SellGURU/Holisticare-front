0f60e25f2241b9f1a3cccc552345ae29b9b41fd6
SmurfsNima <sadeghinima090@gmail.com>
2025-08-11 11:56:01 +0330

    Refactor CustomBranding components for improved layout and responsiveness. Adjust padding in RightItemContent, modify flex direction in TheAppOverview, and standardize text sizes in Toggle for better visual consistency across different screen sizes.

--- src/pages/CustomBranding/components/TheAppOverview.tsx
+++ src/pages/CustomBranding/components/TheAppOverview.tsx
@@ -77,10 +77,10 @@ const TheAppOverview: FC<TheAppOverviewProps> = ({ customTheme }) => {
   const [selectedMeal, setselectedMeal] = useState('Breakfast');
   const [selectedPage, setselectedPage] = useState('Overview');
   return (
-    <div className="w-full flex items-center justify-center gap-8">
+    <div className="w-full flex flex-col md:flex-row items-center justify-center gap-8">
       <div
         style={{ background: gradientWithOpacity }}
-        className="w-[203px] h-[360px] rounded-[12.72px] shadow-400 pt-4 relative flex flex-col items-center justify-center gap-2 select-none"
+        className="  w-[203px] h-[360px] rounded-[12.72px] shadow-400 pt-4 relative flex flex-col items-center justify-center gap-2 select-none"
       >
         {customTheme.selectedImage && (
           <img
