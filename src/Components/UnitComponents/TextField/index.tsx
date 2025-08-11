b404679d703a8cf67f8d7262636dcba6aec8c2f7
amir <amirdehghani137722@gmail.com>
2025-08-11 10:38:22 +0330

    add text field component

--- src/Components/UnitComponents/TextField/index.tsx
+++ src/Components/UnitComponents/TextField/index.tsx
@@ -1,32 +1,42 @@
 interface TextFieldProps {
-    label: string;
-    placeholder: string;
-    value: string;
-    isValid: boolean;
-    validationText: string;
-    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  label: string;
+  placeholder: string;
+  value: string;
+  isValid: boolean;
+  validationText: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-const TextField:React.FC<TextFieldProps> = ({label, placeholder, onChange, value, isValid, validationText}) => {
+const TextField: React.FC<TextFieldProps> = ({
+  label,
+  placeholder,
+  onChange,
+  value,
+  isValid,
+  validationText,
+}) => {
   return (
     <>
-          <div className="flex flex-col mt-5 w-full gap-2">
-            <label htmlFor={"text-field-"+label.toLowerCase().replace(/\s+/g, '-')}  className="text-xs font-medium text-Text-Primary">{label}</label>
-            <input
-              id={"text-field-"+label.toLowerCase().replace(/\s+/g, '-')}
-              placeholder={placeholder}
-              value={value}
-              onChange={onChange}
-              className={`w-full h-[28px] rounded-[16px] py-1 px-3 border ${
-                isValid ? 'border-Red' : 'border-Gray-50'
-              } bg-backgroundColor-Card text-xs font-normal placeholder:text-Text-Fivefold`}
-            />
-            {!isValid && (
-              <div className="text-Red text-[10px]">
-                {validationText}
-              </div>
-            )}
-          </div>    
+      <div className="flex flex-col mt-5 w-full gap-2">
+        <label
+          htmlFor={'text-field-' + label.toLowerCase().replace(/\s+/g, '-')}
+          className="text-xs font-medium text-Text-Primary"
+        >
+          {label}
+        </label>
+        <input
+          id={'text-field-' + label.toLowerCase().replace(/\s+/g, '-')}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+          className={`w-full h-[28px] rounded-[16px] py-1 px-3 border ${
+            isValid ? 'border-Red' : 'border-Gray-50'
+          } bg-backgroundColor-Card text-xs font-normal placeholder:text-Text-Fivefold`}
+        />
+        {!isValid && (
+          <div className="text-Red text-[10px]">{validationText}</div>
+        )}
+      </div>
     </>
   );
 };
