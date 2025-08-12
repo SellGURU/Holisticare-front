cc7722efe6ebe004f68b72037ee1e1321a314dfc
Amirhossein Rahmani <amirrahmani2006@gmail.com>
2025-08-12 15:25:48 +0330

    Add the ability to display additional information and handle key events in the TextAreaField component. Improve label display and add a help widget for additional information.

--- src/Components/UnitComponents/TextAreaField/index.tsx
+++ src/Components/UnitComponents/TextAreaField/index.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import { Tooltip } from 'react-tooltip';
 
 interface TextAreaFieldProps {
   label: string;
@@ -8,6 +9,8 @@ interface TextAreaFieldProps {
   validationText?: string;
   onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
   margin?: string;
+  onKeyDown?: (e: React.KeyboardEvent<HTMLTextAreaElement>) => void;
+  InfoText?: string;
 }
 
 const TextAreaField: FC<TextAreaFieldProps> = ({
@@ -18,10 +21,21 @@ const TextAreaField: FC<TextAreaFieldProps> = ({
   isValid,
   validationText,
   margin,
+  onKeyDown,
+  InfoText,
 }) => {
   return (
     <div className={`flex flex-col w-full gap-2 ${margin ? margin : 'mt-4'}`}>
-      <div className="text-xs font-medium text-Text-Primary">{label}</div>
+      <div className="text-xs font-medium text-Text-Primary flex gap-1 items-start">
+        {label}
+        {InfoText && (
+          <img
+            data-tooltip-id={`info-text-${label.toLowerCase().replace(/\s+/g, '-')}`}
+            src="/icons/info-circle.svg"
+            alt=""
+          />
+        )}
+      </div>
       <textarea
         placeholder={placeholder}
         value={value}
@@ -29,9 +43,19 @@ const TextAreaField: FC<TextAreaFieldProps> = ({
         className={`w-full h-[98px] text-justify rounded-[16px] py-1 px-3 border ${
           isValid ? 'border-Red' : 'border-Gray-50'
         } bg-backgroundColor-Card text-xs font-normal placeholder:text-Text-Fivefold resize-none`}
+        onKeyDown={onKeyDown}
       />
       {validationText && (
         <div className="text-Red text-[10px]">{validationText}</div>
+      )}
+      {InfoText && (
+        <Tooltip
+          id={`info-text-${label.toLowerCase().replace(/\s+/g, '-')}`}
+          place="top-start"
+          className="!bg-white !max-w-[300px] !leading-5 !text-wrap !shadow-100 !text-Text-Primary !text-[10px] !rounded-[6px] !border !border-Gray-50 flex flex-col !z-[99999]"
+        >
+          {InfoText}
+        </Tooltip>
       )}
     </div>
   );
