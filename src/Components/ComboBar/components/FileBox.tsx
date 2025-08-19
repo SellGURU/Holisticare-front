bc56a88f8c717377e9847431b0ac71bb1b7ce85e
amir <amirdehghani137722@gmail.com>
2025-08-19 15:30:21 +0330

    format files

--- src/Components/ComboBar/components/FileBox.tsx
+++ src/Components/ComboBar/components/FileBox.tsx
@@ -120,7 +120,7 @@ const FileBox: React.FC<FileBoxProps> = ({
                   </>
                 ) : (
                   <>
-              {(!el.status || el.status === "completed") && (
+                    {(!el.status || el.status === 'completed') && (
                       <img
                         onClick={() => {
                           if (!isDeleted) {
