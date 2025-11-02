57d97e864fdc9ec2219dcdff6207dcfecc6a8b0a
Amirhossein Rahmani <amirrahmani2006@gmail.com>
2025-11-02 12:15:00 +0330

    refactor: update conditional rendering in ReportAnalyseView to handle HTML report existence and improve share button display logic

--- src/Components/RepoerAnalyse/ReportAnalyseView.tsx
+++ src/Components/RepoerAnalyse/ReportAnalyseView.tsx
@@ -1024,47 +1024,51 @@ const ReportAnalyseView: React.FC<ReportAnalyseViewprops> = ({
                   </div>
                   {TreatMentPlanData?.length > 0 && isHaveReport && !isShare ? (
                     <div className="flex items-center gap-6">
-                      {isShareModalSuccess ? (
-                        <div className="flex flex-col items-center">
-                          <div className="text-Text-Quadruple text-xs font-medium flex items-center gap-1">
-                            <img src="/icons/tick-circle-gray.svg" alt="" />
-                            Shared with Client
-                          </div>
-                          <div className="text-Text-Fivefold text-[10px]">
-                            on{' '}
-                            {dateShare
-                              ? new Date(
-                                  dateShare as string,
-                                ).toLocaleDateString('en-US', {
-                                  month: 'short',
-                                  day: 'numeric',
-                                  year: 'numeric',
-                                })
-                              : 'No date'}
-                          </div>
-                        </div>
-                      ) : (
-                        <div className="rounded-[20px] flex items-center justify-center w-[168px] h-[26px] bg-gradient-to-r from-Primary-DeepTeal to-Primary-EmeraldGreen">
-                          <ButtonPrimary
-                            ClassName="
+                      {!isHtmlReportExists && (
+                        <>
+                          {isShareModalSuccess ? (
+                            <div className="flex flex-col items-center">
+                              <div className="text-Text-Quadruple text-xs font-medium flex items-center gap-1">
+                                <img src="/icons/tick-circle-gray.svg" alt="" />
+                                Shared with Client
+                              </div>
+                              <div className="text-Text-Fivefold text-[10px]">
+                                on{' '}
+                                {dateShare
+                                  ? new Date(
+                                      dateShare as string,
+                                    ).toLocaleDateString('en-US', {
+                                      month: 'short',
+                                      day: 'numeric',
+                                      year: 'numeric',
+                                    })
+                                  : 'No date'}
+                              </div>
+                            </div>
+                          ) : (
+                            <div className="rounded-[20px] flex items-center justify-center w-[168px] h-[26px] bg-gradient-to-r from-Primary-DeepTeal to-Primary-EmeraldGreen">
+                              <ButtonPrimary
+                                ClassName="
       relative z-10 !w-[166px] !h-[24px] !rounded-[20px]
       !bg-backgroundColor-Main !font-medium
       !text-Primary-DeepTeal !text-xs !text-nowrap
       !border-none flex items-center justify-center gap-1
     "
-                            onClick={() => {
-                              // setIsShareModalOpen(true);
-                              publish('openShareModalHolisticPlan', {});
-                            }}
-                          >
-                            <img
-                              src="/icons/document-upload.svg"
-                              alt=""
-                              className="w-4 h-4"
-                            />
-                            Share with Client
-                          </ButtonPrimary>
-                        </div>
+                                onClick={() => {
+                                  // setIsShareModalOpen(true);
+                                  publish('openShareModalHolisticPlan', {});
+                                }}
+                              >
+                                <img
+                                  src="/icons/document-upload.svg"
+                                  alt=""
+                                  className="w-4 h-4"
+                                />
+                                Share with Client
+                              </ButtonPrimary>
+                            </div>
+                          )}
+                        </>
                       )}
 
                       <div className="flex flex-col items-center gap-1">
