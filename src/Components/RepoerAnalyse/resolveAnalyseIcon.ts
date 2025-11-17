a08a250d1842d9ab59e6c484f628a5377af0e9a8
amir <amirdehghani137722@gmail.com>
2025-11-17 15:44:43 +0330

    refactor: improve formatting and consistency in resolveAnalyseIcon function for better readability

--- src/Components/RepoerAnalyse/resolveAnalyseIcon.ts
+++ src/Components/RepoerAnalyse/resolveAnalyseIcon.ts
@@ -8,7 +8,7 @@ const resolveAnalyseIcon = (name: string) => {
   if (name == 'Diabetes & Glucose') {
     return '/icons/biomarkers/donor.svg';
   }
-  if ( name == 'Immune Health') {
+  if (name == 'Immune Health') {
     return '/icons/biomarkers/Inflammation.svg';
   }
   if (name == 'Phenotypic Age') {
@@ -77,10 +77,13 @@ const resolveAnalyseIcon = (name: string) => {
   if (name == 'Vitamins') {
     return '/icons/biomarkers/vitamins.svg';
   }
-  if (name == 'Inflammation & Coagulation' || name == 'Immune Health and Inflammation') {
+  if (
+    name == 'Inflammation & Coagulation' ||
+    name == 'Immune Health and Inflammation'
+  ) {
     return '/icons/biomarkers/cell.svg';
   }
-  if(name == 'Hormone Health'){
+  if (name == 'Hormone Health') {
     return '/icons/biomarkers/brain_02.svg';
   }
   if (name == 'Trace Essential Minerals') {
