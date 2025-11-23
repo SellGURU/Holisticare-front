6c6e8d67b8f77afce6fd09c4f1601582d34f88d0
Amirhossein Rahmani <amirrahmani2006@gmail.com>
2025-11-23 17:35:30 +0330

    feat: implement update and fillout tracking in FullScreenModal with asynchronous checks for questionnaire status

--- src/Components/ComboBar/FullScreenModal.tsx
+++ src/Components/ComboBar/FullScreenModal.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState } from 'react';
 import { publish, subscribe } from '../../utils/event';
+import Application from '../../api/app';
 
 const FullScreenModal = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,6 +12,52 @@ const FullScreenModal = () => {
   });
   window.addEventListener('message', (event) => {
     if (event.data.type === 'closeFullscreenModal') {
+      const receivedData = event.data.data;
+      if (receivedData.isUpdate) {
+        publish('openUpdateQuestionnaireTrackingProgressModal', {});
+        const checkUpdate = async () => {
+          try {
+            const res = await Application.checkUpdateQuestionary({
+              f_unique_id: receivedData.f_unique_id as string,
+              q_unique_id: receivedData.q_unique_id as string,
+              member_id: receivedData.member_id as string,
+            });
+            if (res.status === 200 && res.data.status === true) {
+              publish('UpdateQuestionnaireTrackingSuccess', {});
+            } else {
+              setTimeout(checkUpdate, 15000);
+            }
+          } catch (err) {
+            console.error('err', err);
+
+            setTimeout(checkUpdate, 15000);
+          }
+        };
+        checkUpdate();
+      }
+      if (receivedData.isFill) {
+        publish('openFilloutQuestionnaireTrackingProgressModal', {});
+        const checkFillout = async () => {
+          try {
+            const res = await Application.checkFilloutQuestionary({
+              f_unique_id: receivedData.f_unique_id as string,
+              q_unique_id: receivedData.q_unique_id as string,
+              member_id: receivedData.member_id as string,
+            });
+            if (res.status === 200 && res.data.status === true) {
+              publish('FilloutQuestionnaireTrackingSuccess', {});
+            } else {
+              setTimeout(checkFillout, 15000);
+            }
+          } catch (err) {
+            console.error('err', err);
+
+            setTimeout(checkFillout, 15000);
+          }
+        };
+        checkFillout();
+      }
+
       setIsOpen(false);
       publish('reloadQuestionnaires', {});
       setUrl('');
