74bdab662606f4c73e6a1c23535aff67f11cf55f
Taraneh-jalalinia <taraneh.jalalinia@gmail.com>
2026-05-30 15:15:18 +0000

    Refactor API endpoint configuration for improved clarity
    - Reorganize the structure of the base API endpoint definitions in base.ts for better readability.
    - Maintain consistent formatting and indentation throughout the file.
    - Ensure the logic for resolving base endpoints and URLs remains intact while enhancing code clarity.

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,32 +1,41 @@
-/* eslint-disable prefer-const */
-const baseProductEndPoint =
-  'https://vercel-backend-one-roan.vercel.app/holisticare';
-const baseTestEndPoint =
-  'https://vercel-backend-one-roan.vercel.app/holisticare_test';
-
-const defaultLocalApiUrl = 'http://127.0.0.1:3901';
-const baseLocalEndpoint =
-  (import.meta.env.VITE_API_URL as string | undefined)?.replace(/\/$/, '') ||
-  defaultLocalApiUrl;
-
-const baseProductUrl = 'https://holisticare.vercel.app';
-const baseTestUrl = 'https://holisticare-develop.vercel.app';
-let env: 'test' | 'production' | 'local' = 'production';
-
-const resolveBaseEndPoint = () => {
-  if (env == 'local') {
-    return baseLocalEndpoint;
-  }
-  if (env == 'test') {
-    return baseTestEndPoint;
-  }
-  return baseProductEndPoint;
-};
-const resolveBaseUrl = () => {
-  if (env === 'test') {
-    return baseTestUrl;
-  }
-  return baseProductUrl;
-};
-
-export { resolveBaseEndPoint, resolveBaseUrl, env };
+/* eslint-disable prefer-const */
+
+const baseProductEndPoint =
+  'https://vercel-backend-one-roan.vercel.app/holisticare';
+
+const baseTestEndPoint =
+  'https://vercel-backend-one-roan.vercel.app/holisticare_test';
+
+const defaultLocalApiUrl = 'http://127.0.0.1:3901';
+
+const baseLocalEndpoint =
+  (import.meta.env.VITE_API_URL as string | undefined)?.replace(/\/$/, '') ||
+  defaultLocalApiUrl;
+
+const baseProductUrl = 'https://holisticare.vercel.app';
+
+const baseTestUrl = 'https://holisticare-develop.vercel.app';
+
+let env: 'test' | 'production' | 'local' = 'production';
+
+const resolveBaseEndPoint = () => {
+  if (env == 'local') {
+    return baseLocalEndpoint;
+  }
+
+  if (env == 'test') {
+    return baseTestEndPoint;
+  }
+
+  return baseProductEndPoint;
+};
+
+const resolveBaseUrl = () => {
+  if (env === 'test') {
+    return baseTestUrl;
+  }
+
+  return baseProductUrl;
+};
+
+export { resolveBaseEndPoint, resolveBaseUrl, env };
